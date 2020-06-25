13bdfd96701774e86314e0ac693825a8b864df7e
Finley Lau <finleylau98@gmail.com>
2020-06-25 14:35:32 -0500

    client: initial commit of cra

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import VideoStimulus from './components/PassiveStimulus/VideoStimulus'
+
+function App() {
+  return (
+    <div className="App">
+      <VideoStimulus />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/PassiveStimulus/VideoStimulus/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Row, Col, Button, Form} from 'react-bootstrap';
+
+export default class VideoStimulus extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>hi</div>
+        )
+    }
+
+}
