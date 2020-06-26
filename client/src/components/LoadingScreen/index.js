79ce4f0ad562601d662bbeb71f99f18016f6a015
Finley Lau <finleylau98@gmail.com>
2020-06-26 12:23:44 -0500

    client: single video stimulus page

---
+++ client/src/components/LoadingScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingScreen() {
+  return (
+    <div className="loader centered-content">
+      <i className="fa fa-cog fa-spin" />
+    </div>
+  );
+}
+
+export default LoadingScreen;
